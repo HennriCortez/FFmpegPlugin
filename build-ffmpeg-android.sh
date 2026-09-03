fd0388ee4dc32e119df842ba0f49d2873c153449
Zetroch <hennricelardocortez@gmail.com>
2026-09-03 21:26:04 +0800

    Enable MediaCodec in plugin APK workflow

--- build-ffmpeg-android.sh
+++ build-ffmpeg-android.sh
@@ -105,6 +105,7 @@ echo "==> Configuring ffmpeg"
   --enable-cross-compile \
   --enable-static \
   --disable-shared \
+  --enable-mediacodec \
   --pkg-config=pkg-config \
   --pkg-config-flags="--static" \
   --enable-openssl \
